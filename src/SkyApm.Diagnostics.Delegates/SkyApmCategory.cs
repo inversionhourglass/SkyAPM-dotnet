0b6d456dd31b4b7ab119ede47c880e2c7bcd2714
ihourglass <inversionhourglass@outlook.com>
2023-03-03 20:45:47 +0800

    Add SkyApm.Diagnostics.Delegates

---
+++ src/SkyApm.Diagnostics.Delegates/SkyApmCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace SkyApm.Diagnostics.Delegates
+{
+    public static class SkyApmCategory
+    {
+        public const string CLEAN_CONTEXT = "ClearContext";
+
+        public const string WEAKEN_CONTEXT = "WeakenContext";
+    }
+}
---
+++ src/SkyApm.Diagnostics.Delegates/SkyWalkingBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SkyApm.Utilities.DependencyInjection;
+using System;
+
+namespace SkyApm.Diagnostics.Delegates
+{
+    public static class SkyWalkingBuilderExtensions
+    {
+        public static SkyApmExtensions AddDelegates(this SkyApmExtensions extensions)
+        {
+            if (extensions == null)
+            {
+                throw new ArgumentNullException(nameof(extensions));
+            }
+
+            extensions.Services.AddSingleton<ITracingDiagnosticProcessor, DelegateDiagnosticProcessor>();
+
+            return extensions;
+        }
+    }
+}
